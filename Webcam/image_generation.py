e85a16c5cf66358aebcad138b05ffff4a170903e
CHOI DONGHYEOK <39021804+HSClover@users.noreply.github.com>
2025-11-08 12:47:41 +0900

    upload picamera

---
+++ Webcam/image_generation.py
@@ -0,0 +1,12 @@
+import time
+import picamera
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.IN, GPIO.PUD_UP) # button; pull-up register
+
+with picamera.PiCamera() as cam:
+    cam.start_preview()
+    GPIO.wait_for_edge(18,GPIO.FALLING)
+    cam.capture('/home/pi/photo/image.jpg)
+    cam.stop_preview() 
